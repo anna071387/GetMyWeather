5ced4a706bcee47332392c23e771dce9da00dfbe
Anna Gallagher <annagallagher713@gmail.com>
2023-05-25 22:47:44 -0600

    added styles, css, readme file

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# GetMyWeather
+# GetMyWeather 
+
+![alt text](./assets/images/MySky.jpg.jpg)
+
+## Description 
+This is a weather app that allow to search for any city and discover what weather is currently in that city. 
+&Day forecast provided
+
+## Use 
+The added search button allows to connect data for any particular city and represent the today temperature, 
+see the icon for what type of weather is, wind speed, humidity.
+
+Search button allow for easy search and searched cities are stored at the bottom of the forecast 
+with all the information available to view at glance. 
+
+# Results
+During the creation of the app I have learnt how to fetch API from the weather source (rest API). 
+I have learnt how to access arrays to retrieve information that I want to represent on my website/app.
+I applied styling methods to create visual features. 
+DayJs methods for date retrieval
+
+
+
+Inspiration: 
+Web Dev Simplified: Weather app
+
+
--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,139 @@
-async function getLatLon(query) {
-    var requestUrl =`https://api.openweathermap.org/geo/1.0/direct?q=${query}&=imperial`;
-    try {
-       var location = await fetch(requestUrl)
-       var data = await location.json()
-       return {
-        lon: data[0].lon,
-        lat: data[0].lat
-    }
-    } catch (error) {
-        
-    }
-  
-    }
 
 
-function getWeather (){
-    var weatherContainer = document.querySelector('weather')
-    
+// https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,relativehumidity_2m,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,windspeed_10m_max&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FChicago
+
+export function getWeather(lat, lon, timezone) {
+
 }
 
-(async function() {
-     console.log(await getLatLon('Boston'));
-
-})();
-// getLatLon('Boston');
 
 
 
 
 
-getWeather();
+
+
+
+
+//  Allows to draw data for a searched city, api key applied
+// function fetchWeather(query){
+    
+    // draws response from data
+    function fetchWeather(requestUrl) {
+
+    var requestUrl = `https://api.openweathermap.org/data/2.5/forcast?q=Boston&appid=784613bb935644a3c30729d18d748757&units=imperial`;
+
+    fetch(requestUrl)
+        .then(function(response) {
+        return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        })
+
+        .catch(function(err) {
+            console.log(err);
+        })
+    }
+ 
+// call function
+
+
+// separates data results
+//  function renderWeather (weather) {
+//     var resultsContainer =document.querySelector("#weather-results");
+//     var city =document.createElement('h2');
+//     city.textContent = weather.name;
+//     resultsContainer.append(city);
+//     // City, humidity, temperature, wind
+
+//     var temp=document.createElement('p');
+//     temp.textContent = 'Temp: ' + weather.main.temp + "F";
+//     resultsContainer.append(temp);
+
+//     var humidity=document.createElement('p');
+//     humidity.textContent = 'Humidity: ' + weather.main.humidity + "%";
+//     resultsContainer.append(humidity);
+
+//     var wind=document.createElement('p');
+//     wind.textContent = 'Wind: ' + weather.wind.speed + "mph";
+//     resultsContainer.append(wind);
+
+    
+//     details.append('')
+
+//  }
+
+
+ fetchWeather();
+//  renderWeather();
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var city= document.getElementById('')
+
+// async function getLatLon(query) {
+//     var requestUrl =`https://api.openweathermap.org/geo/1.0/direct?q=${query}&=imperial`;
+//     try {
+//        var location = await fetch(requestUrl)
+//        var data = await location.json()
+//        return {
+//         lon: data[0].lon,
+//         lat: data[0].lat
+//     }
+//     } catch (error) {
+
+//    }
+//     }
+
+
+// function getWeather (){
+//     var weatherContainer = document.querySelector('weather')
+    
+// }
+
+// (async function() {
+//      console.log(await getLatLon(city));
+// })();
+
+
+
+
+
+
+
+
+// getWeather();
 // getForcast(); 
 
 // renderForcast();
